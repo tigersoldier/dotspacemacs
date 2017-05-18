155c2b7f38c3512ee328f6405285d9ada12a7479
Caibin Chen <tigersoldi@gmail.com>
2017-05-18 00:17:12 -0700

    Add configuration for JavaComp jump to definition.

--- javacomp/packages.el
+++ javacomp/packages.el
@@ -20,36 +20,38 @@
 (defun javacomp/init-javacomp ()
   (use-package javacomp
     :defer t
-    :commands (javacomp-mode company-javacomp)
+    :commands (javacomp-mode company-javacomp javacomp-jump-to-definition)
     :init
     (progn
+      (evilified-state-evilify javacomp-references-mode javacomp-references-mode-map
+        (kbd "C-k") 'javacomp-find-previous-location
+        (kbd "C-j") 'javacomp-find-next-location
+        (kbd "C-l") 'javacomp-goto-location)
       (add-hook 'java-mode-hook 'javacomp-mode)
       ;; (add-hook 'java-mode-hook 'eldoc-mode)
 
-      ;; (add-to-list 'spacemacs-jump-handlers-java-mode 'javacomp-jump-to-definition)
+      (add-to-list 'spacemacs-jump-handlers-java-mode 'javacomp-jump-to-definition)
 
       (push 'company-javacomp company-backends-java-mode))
     :config
     (progn
       (setq javacomp-options-log-path "/tmp/javacomp.log"
             javacomp-options-log-level "fine"))
-    ;;   ;; (spacemacs/declare-prefix-for-mode 'java-mode "mg" "goto")
-    ;;   ;; (spacemacs/declare-prefix-for-mode 'java-mode "mh" "help")
-    ;;   ;; (spacemacs/declare-prefix-for-mode 'java-mode "mn" "name")
-    ;;   ;; (spacemacs/declare-prefix-for-mode 'java-mode "mr" "rename")
-    ;;   ;; (spacemacs/declare-prefix-for-mode 'java-mode "mS" "server")
-    ;;   ;; (spacemacs/declare-prefix-for-mode 'java-mode "ms" "send")
+      (spacemacs/declare-prefix-for-mode 'java-mode "mg" "goto")
+      ;; (spacemacs/declare-prefix-for-mode 'java-mode "mh" "help")
+      ;; (spacemacs/declare-prefix-for-mode 'java-mode "mn" "name")
+      ;; (spacemacs/declare-prefix-for-mode 'java-mode "mr" "rename")
+      (spacemacs/declare-prefix-for-mode 'java-mode "mS" "server")
+      ;; (spacemacs/declare-prefix-for-mode 'java-mode "ms" "send")
 
-    ;;   (defun java/jump-to-type-def()
-    ;;     (interactive)
-    ;;     (javacomp-jump-to-definition t))
-
-    ;;   (spacemacs/set-leader-keys-for-major-mode 'java-mode
-    ;;     "gb" 'javacomp-jump-back
-    ;;     "gt" 'java/jump-to-type-def
-    ;;     "gu" 'javacomp-references
-    ;;     "hh" 'javacomp-documentation-at-point
-    ;;     "rr" 'javacomp-rename-symbol
-    ;;     "Sr" 'javacomp-restart-server))
-    ))
+      (spacemacs/set-leader-keys-for-major-mode 'java-mode
+        ;; "gb" 'javacomp-jump-back
+        "gt" 'javacomp-jump-to-definition
+        ;; "gu" 'javacomp-references
+        ;; "hh" 'javacomp-documentation-at-point
+        ;; "rr" 'javacomp-rename-symbol
+        "Sr" 'javacomp-restart-server
+        )
+      )
+    )
 ;;; packages.el ends here
