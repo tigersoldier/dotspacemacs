d9720cdd620e27c9a5d9bbaca839737e3d1e4778
Caibin Chen <tigersoldi@gmail.com>
2017-11-01 09:08:50 -0700

    Improve javacomp compilateion

--- javacomp/funcs.el
+++ javacomp/funcs.el
@@ -12,21 +12,26 @@
 
 ;;; Code:
 
+(defun spacemacs//javacomp-bazel-sentinel (process state)
+  (message "javacomp build: %s" state)
+  (when (equal state "finished\n")
+    (let ((dest-jar (expand-file-name "~/.emacs.d/javacomp/javacomp.jar")))
+      (copy-file (expand-file-name "~/code/JavaComp/bazel-bin/src/main/java/org/javacomp/server/JavaComp_deploy.jar")
+                 dest-jar
+                 t)
+      (chmod dest-jar #o644)
+      (when (functionp 'javacomp-restart-server)
+        (javacomp-restart-server))
+      (message "JavaComp is successfully installed"))))
+
 (defun spacemacs/build-javacomp ()
   "Rebuild JavaComp server."
   (interactive "*")
 
-  (let ((default-directory (expand-file-name "~/code/JavaComp/"))
-        (dest-jar (expand-file-name "~/.emacs.d/javacomp/javacomp.jar")))
-    (if (= 0 (call-process "bazel" nil "*javacomp-rebuild*" nil
-                           "build"
-                           "src/main/java/org/javacomp/server/JavaComp_deploy.jar"))
-        (progn
-          (copy-file "bazel-bin/src/main/java/org/javacomp/server/JavaComp_deploy.jar"
-                     dest-jar
-                     t)
-          (chmod dest-jar #o644)
-          (when (functionp 'javacomp-restart-server)
-            (javacomp-restart-server))
-          (message "JavaComp is successfully built"))
-      (message "Fail to build JavaComp. Switch to *javacomp-rebuild* buffer to see the build results."))))
+  (let ((default-directory (expand-file-name "~/code/JavaComp/")))
+    (compile "bazel build src/main/java/org/javacomp/server/JavaComp_deploy.jar")
+    (with-current-buffer "*compilation*"
+      (when (get-buffer "*javacomp-rebuild*")
+        (kill-buffer "*javacomp-rebuild*"))
+      (rename-buffer "*javacomp-rebuild*")
+      (set-process-sentinel (get-buffer-process (current-buffer)) #'spacemacs//javacomp-bazel-sentinel))))
