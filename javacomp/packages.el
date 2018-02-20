e5c0c35c60ceb0effb0a603d202c030574551e14
Caibin Chen <tigersoldi@gmail.com>
2018-02-19 20:36:45 -0800

    Update javacomp configs.
     - Require company-lsp before enabling javacomp.
     - Use :command to lazy-loading the package.

--- javacomp/packages.el
+++ javacomp/packages.el
@@ -17,23 +17,17 @@
     (lsp-javacomp :requires lsp-mode)))
 
 (defun javacomp/init-lsp-javacomp ()
-  (use-package lsp-mode
-    :defer t
-    :config (require 'lsp-javacomp))
-
   (use-package lsp-javacomp
-    :defer t
+    :commands lsp-javacomp-enable
     :init
-    (progn
-      (add-hook 'java-mode-hook
-                (lambda ()
-                  (require 'lsp-javacomp)
-                  (make-variable-buffer-local 'company-backends)
-                  (setq company-backends '(company-lsp))
-                  (lsp-javacomp-enable)
-                  (set (make-variable-buffer-local 'company-idle-delay) 0)
-                  (set (make-variable-buffer-local 'company-minimum-prefix-length) 1)
-                  )))
-    :config (lsp-javacomp-install-server)))
+    (add-hook 'java-mode-hook
+              (lambda ()
+                (require 'company-lsp)
+                (lsp-javacomp-enable)
+                (set (make-variable-buffer-local 'company-backends) '(company-lsp))
+                (set (make-variable-buffer-local 'company-idle-delay) 0.1)
+                (set (make-variable-buffer-local 'company-minimum-prefix-length) 1)))
+    :config
+    (lsp-javacomp-install-server)))
 
 ;;; packages.el ends here
